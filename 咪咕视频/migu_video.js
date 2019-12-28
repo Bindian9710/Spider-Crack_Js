94ab650511a1cc249bb232a5f9398e49adab9ed6
mayn <nnlcccc@outlook.com>
2019-12-28 15:53:43 +0800

    修改文件夹为中文

--- 咪咕视频/migu_video.js
+++ 咪咕视频/migu_video.js
@@ -459,7 +459,7 @@ if (null == qb) {
 ab.prototype.nextBytes = _, db.prototype.doPublic = fb, db.prototype.setPublic = eb, db.prototype.encrypt = gb, RSAKey = db
 
 
-function getPwd(a) {
+function getPwd(pwd) {
     c = new RSAKey;
     c.setPublic(a.result.modulus, a.result.publicExponent);
     var d = c.encrypt(pwd);
@@ -486,7 +486,6 @@ var a = {
             "modulus": "00833c4af965ff7a8409f8b5d5a83d87f2f19d7c1eb40dc59a98d2346cbb145046b2c6facc25b5cc363443f0f7ebd9524b7c1e1917bf7d849212339f6c1d3711b115ecb20f0c89fc2182a985ea28cbb4adf6a321ff7e715ba9b8d7261d1c140485df3b705247a70c28c9068caabbedbf9510dada6d13d99e57642b853a73406817"
         }
     };
-getPwd(a);
-var b = rsaFingerprint(a.result.modulus, a.result.publicExponent);
+var password = getPwd(a);
 
-get_fingerPrint_and_fingerPrintDetail(a,b)
+var fingerPrint_and_fingerPrintDetail = get_fingerPrint_and_fingerPrintDetail(a.result.modulus, a.result.publicExponent)
