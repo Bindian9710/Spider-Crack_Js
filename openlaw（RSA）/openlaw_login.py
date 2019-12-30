84a381bf6804657877666c84203059e471b98dc1
mayn <nnlcccc@outlook.com>
2019-12-30 17:57:15 +0800

    openlaw demo

--- openlaw（RSA）/openlaw_login.py
+++ openlaw（RSA）/openlaw_login.py
@@ -17,7 +17,7 @@ with open('./openlaw_login.js','r',encoding='utf-8') as f:
 session = requests.session()
 
 keyEncrypt_password = login_js.call('keyEncrypt',password)
-login_url = 'http://openlaw.cn/login'
+login_url = 'http://openlaw（RSA）.cn/login'
 
 raw_headers = '''Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
 Accept-Encoding: gzip, deflate
@@ -26,14 +26,14 @@ Cache-Control: max-age=0
 Connection: keep-alive
 Content-Length: 526
 Content-Type: application/x-www-form-urlencoded
-Host: openlaw.cn
-Origin: http://openlaw.cn
-Referer: http://openlaw.cn/login.jsp?logout
+Host: openlaw（RSA）.cn
+Origin: http://openlaw（RSA）.cn
+Referer: http://openlaw（RSA）.cn/login.jsp?logout
 Upgrade-Insecure-Requests: 1
 User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'''
 headers = dict([line.split(": ",1) for line in raw_headers.split("\n")])
 
-res = session.get('http://openlaw.cn/login.jsp?logout')
+res = session.get('http://openlaw（RSA）.cn/login.jsp?logout')
 csrf = re.compile(r'csrf" value="(.*?)"')
 
 formdata = {
@@ -45,6 +45,6 @@ formdata = {
 
 _csrf_resp = session.post(login_url,headers=headers,data=formdata,timeout=10,allow_redirects=False)
 
-login_result = session.get('http://openlaw.cn/user/profile.jsp')
+login_result = session.get('http://openlaw（RSA）.cn/user/profile.jsp')
 if userName in login_result.text:
     print('登陆成功!')
